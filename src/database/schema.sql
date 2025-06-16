21df38ab8c536ce4dfcada58c206a2cfae6b1dc5
Mikael Morais <yagopx3@gmail.com>
2025-06-16 13:43:46 -0300

    att_api

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,7 +1,16 @@
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE triagem (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    usuario_id INTEGER NOT NULL,
+    dor_cabeca TEXT CHECK(dor_cabeca IN ('sim', 'não')),
+    -- Adicione outras perguntas conforme necessário
+    data_resposta TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (usuario_id) REFERENCES users(id)
 );
